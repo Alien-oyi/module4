887aa431ae660530887c86f61ce3717e9b79b40e
Alien-oyi <daijinmin78@gmail.com>
2023-02-05 19:39:19 -0500

    apply css

---
+++ java.js
@@ -0,0 +1,125 @@
+{
+    "response_code": 0,
+    "results": [
+        {
+            "category": "Entertainment: Comics",
+            "correct_answer": "Thomas &amp; Martha",
+            "difficulty": "medium",
+            "incorrect_answers": [
+                "Joey &amp; Jackie",
+                "Jason &amp; Sarah",
+                "Todd &amp; Mira"
+            ],
+            "question": "What&#039;s the name of Batman&#039;s parents?",
+            "type": "multiple"
+        },
+        {
+            "category": "Entertainment: Film",
+            "correct_answer": "Bruce Lee",
+            "difficulty": "easy",
+            "incorrect_answers": [
+                "Jackie Chan",
+                "Jet Li",
+                " Yun-Fat Chow"
+            ],
+            "question": "Who starred in the film 1973 movie &quot;Enter The Dragon&quot;?",
+            "type": "multiple"
+        },
+        {
+            "category": "Science: Computers",
+            "correct_answer": "50",
+            "difficulty": "hard",
+            "incorrect_answers": [
+                "59",
+                "60",
+                "25"
+            ],
+            "question": "How many Hz does the video standard PAL support?",
+            "type": "multiple"
+        },
+        {
+            "category": "Geography",
+            "correct_answer": "Sweden",
+            "difficulty": "medium",
+            "incorrect_answers": [
+                "Denmark",
+                "Norway",
+                "Germany"
+            ],
+            "question": "The land of Gotland is located in which European country?",
+            "type": "multiple"
+        },
+        {
+            "category": "History",
+            "correct_answer": "Guy Fawkes",
+            "difficulty": "medium",
+            "incorrect_answers": [
+                "Robert Catesby",
+                "Francis Tresham",
+                "Everard Digby"
+            ],
+            "question": "Which infamous European traitor was known as &quot;the last person to enter Parliament with honest intentions&quot;?",
+            "type": "multiple"
+        },
+        {
+            "category": "History",
+            "correct_answer": "July 16, 1945",
+            "difficulty": "medium",
+            "incorrect_answers": [
+                "June 22, 1945",
+                "August 6, 1945",
+                "April 5, 1945"
+            ],
+            "question": "When was &quot;The Gadget&quot;, the first nuclear device to be detonated, tested?",
+            "type": "multiple"
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "correct_answer": "Markus Persson",
+            "difficulty": "easy",
+            "incorrect_answers": [
+                "Jens Bergensten",
+                "Daniel Rosenfeld",
+                "Carl Manneh"
+            ],
+            "question": "Who created the 2011 Video Game &quot;Minecraft&quot;?",
+            "type": "multiple"
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "correct_answer": "Singing into a Microphone",
+            "difficulty": "hard",
+            "incorrect_answers": [
+                "Playing a Piano",
+                "Using a Composer Software",
+                "Listened to birds at the park"
+            ],
+            "question": "According to Toby Fox, what was the method to creating the initial tune for Megalovania?",
+            "type": "multiple"
+        },
+        {
+            "category": "Art",
+            "correct_answer": "Copic",
+            "difficulty": "medium",
+            "incorrect_answers": [
+                "Dopix",
+                "Cofix",
+                "Marx"
+            ],
+            "question": "Which of these are the name of a famous marker brand?",
+            "type": "multiple"
+        },
+        {
+            "category": "Entertainment: Music",
+            "correct_answer": "1993",
+            "difficulty": "hard",
+            "incorrect_answers": [
+                "1992",
+                "1995",
+                "1994"
+            ],
+            "question": "What year was Min Yoongi from South Korea boy band &quot;BTS&quot; born in?",
+            "type": "multiple"
+        }
+    ]
+}
